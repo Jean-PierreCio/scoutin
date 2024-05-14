34ebceee8fab40e5d991b061d563379b9e6e8b89
Jean-Pierre Ciotta <89547867+Jean-PierreCio@users.noreply.github.com>
2024-05-13 17:54:33 -0700

    fresh

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
 // this is where we write our backend node.js code
+var express = require("express");
+//var cors = require("cors");
+var mysql = require("mysql");
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hey There!');
+});
+
+// app.use(cors());
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
